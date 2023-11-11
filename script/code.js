7164329455042ce474459dbdbc2aeb5cbad57516
D-Raff <damonraffels@gmail.com>
2023-11-11 11:17:41 +0200

    declared some variables

--- script/code.js
+++ script/code.js
@@ -1,1 +1,10 @@
-// code
+const numButtons = document.querySelectorAll('[data-number]')
+const actionButtons = document.querySelectorAll('[data-action]')
+const equalsBtn = document.querySelector('[data-equals]')
+const clearBtn = document.querySelector('[data-clear]')
+const delBtn = document.querySelector('[data-delete]')
+const Current = document.querySelector('[data-current]')
+const Result = document.querySelector('[data-res]')
+
+let currDisplay = "0";
+let resDisplay = false;
