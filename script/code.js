0f8a26116f8272972bbc63a619028a0cc09aba06
D-Raff <damonraffels@gmail.com>
2023-11-13 20:26:02 +0200

    did functionality with peers

--- script/code.js
+++ script/code.js
@@ -1,1 +1,47 @@
-// JS
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("[data-value]");
+let operators = ["+", "-", "*", "/", "."];
+
+let currentInput = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const value = button.getAttribute("data-value");
+
+    if (operators.includes(value)) {
+      if (currentInput.includes(value)) {
+        return;
+      }
+    }
+
+    if (value === "=") {
+      try {
+        currentInput = eval(currentInput);
+
+        display.value = currentInput;
+      } catch (error) {
+        display.value = "Error";
+      }
+    } else if (value === "C") {
+      currentInput = "";
+
+      display.value = "";
+
+      operators.forEach((operator) => {
+        const operatorButton = document.querySelector(
+          `[data-value="${operator}"]`
+        );
+
+        operatorButton.disabled = false;
+      });
+    } else {
+      currentInput += value;
+
+      display.value = currentInput;
+
+      if (operators.includes(value)) {
+        button.disabled = false;
+      }
+    }
+  });
+});
