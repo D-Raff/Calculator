e400ca2686c8af88b92f3f2623b3ce3a1e0c1f40
D-Raff <damonraffels@gmail.com>
2023-11-13 20:06:01 +0200

    completely redid css and values of html

--- script/code.js
+++ script/code.js
@@ -1,12 +1,1 @@
-const numButtons = document.querySelectorAll('[data-number]')
-const actionButtons = document.querySelectorAll('[data-action]')
-const equalsBtn = document.querySelector('[data-equals]')
-const clearBtn = document.querySelector('[data-clear]')
-const delBtn = document.querySelector('[data-delete]')
-const Current = document.querySelector('[data-current]')
-const Result = document.querySelector('[data-res]')
-
-function appendToDisplay() {
-}
-
-numButtons.addEventListener('click', appendToDisplay)
+// JS
